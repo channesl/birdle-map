0372b664fed1184a245b029893c3df633fd46364
Hannes Lindström <hannesxlindstrom@gmail.com>
2026-04-16 13:53:43 +0200

    Project init with vite, react, ts, tailwind

---
+++ birdle/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-slate-50">
+      <h1 className="text-3xl font-bold text-blue-600">Birdle Map Loading...</h1>
+    </div>
+  )
+}
+
+export default App
